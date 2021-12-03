fd8c819bfff6baefe039f387de444011dbafa489
Antonis Keremidis <tkeremidis@gmail.com>
2021-12-03 18:46:22 +0200

    Fix RTT, Compile ecss-services to ARM

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,7 @@
 #include "main.h"
-#include "Services/EventReportService.hpp"
 #include "SEGGER_RTT.h"
 
 extern "C" void main_cpp() {
-    EventReportService eventReportService = EventReportService();
-    const char eventReportData[] = "HelloWorld";
-    eventReportService.lowSeverityAnomalyReport(EventReportService::LowSeverityUnknownEvent, eventReportData);
+    SEGGER_RTT_Init();
     SEGGER_RTT_printf(0, "test");
 }
