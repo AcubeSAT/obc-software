0d8cc4246342afabb61138f63bdf6d6d1a8d95bf
Antonis Keremidis <tkeremidis@gmail.com>
2021-12-03 21:05:40 +0200

    Add platform cpp files to be implemented, use st[05] in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,11 @@
 #include "main.h"
 #include "SEGGER_RTT.h"
+#include "ServicePool.hpp"
 
 extern "C" void main_cpp() {
     SEGGER_RTT_Init();
     SEGGER_RTT_printf(0, "test");
+    EventReportService &eventReportService = Services.eventReport;
+    eventReportService.lowSeverityAnomalyReport(EventReportService::LowSeverityUnknownEvent, "data");
+    SEGGER_RTT_printf(0, "%i", eventReportService.lowSeverityEventCount);
 }
