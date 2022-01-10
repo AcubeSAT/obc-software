e67efe0eeac9334be10af20003646f2951a0715a
Antonis Keremidis <tkeremidis@gmail.com>
2022-01-10 15:30:33 +0200

    Revert main.c

--- src/main.c
+++ src/main.c
@@ -35,21 +35,10 @@
 // *****************************************************************************
 
 
-void USART1_Callback(uintptr_t context) {
-    if (USART1_ErrorGet() != USART_ERROR_NONE) {
-        //Handle error case
-    } else {
-        //Transfer completed successfully
-    }
-}
-
 int main(void) {
-    char data[17] = "\r\n testing \r\n";
-    USART1_Initialize();
-    USART1_WriteCallbackRegister(USART1_Callback, (uintptr_t) NULL);
-    USART1_Write(&data, 17);
+    /* Initialize all modules */
 
-//    main_cpp();
+    main_cpp();
 
     /* Execution should not come here during normal operation */
 
