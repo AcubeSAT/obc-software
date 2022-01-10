8d453e26726c646f8d24bf43f4ea5944161c0817
Antonis Keremidis <tkeremidis@gmail.com>
2022-01-10 15:44:06 +0200

    revert main.cpp, add showcase task for USART

--- src/main.cpp
+++ src/main.cpp
@@ -8,14 +8,6 @@
 #include "task.h"
 #include "definitions.h"
 
-/**
- * @note This should not be considered a final main.cpp file and will be reverted.
- * Its purpose is just to test USART1 functionality.
- *
- * PIO_PIN_PB4 is the USART1 TX pin according to page 1140 of the microcontroller datasheet.
- * pinval should be toggling while sending a message,
- * however this implementation does not work at the moment.
- */
 
 volatile uint8_t pinval = 0;
 etl::string<17> usartData = "\r\n testing \r\n";
@@ -29,7 +21,7 @@ void xTask1Code(void *pvParameters) {
 
 void xTask2Code(void *pvParameters) {
     while (true) {
-//        pinval = PIO_PinRead(PIO_PIN_PA23);
+        pinval = PIO_PinRead(PIO_PIN_PA23);
         vTaskDelay(pdMS_TO_TICKS(500));
     }
 };
@@ -37,11 +29,7 @@ void xTask2Code(void *pvParameters) {
 void xUartDMA(void *pvParameters) {
     while (true) {
         USART1_Write(usartData.data(), usartData.size());
-        while (USART1_WriteIsBusy()) {
-            pinval = PIO_PinRead(PIO_PIN_PB4);
-            SEGGER_RTT_printf(0, "\r\nbusy\r\n");
-        }
-        vTaskDelay(pdMS_TO_TICKS(1000));
+        vTaskDelay(pdMS_TO_TICKS(3000));
     }
 };
 
@@ -53,8 +41,8 @@ extern "C" void main_cpp() {
     eventReportService.lowSeverityAnomalyReport(EventReportService::LowSeverityUnknownEvent, "data");
     uint8_t lowSeverityEvents = eventReportService.lowSeverityEventCount; //Variable to check in ozone timeline
 
-//    xTaskCreate(xTask1Code, "Task1", 100, NULL, tskIDLE_PRIORITY + 1, NULL);
-//    xTaskCreate(xTask2Code, "Task2", 100, NULL, tskIDLE_PRIORITY + 1, NULL);
+    xTaskCreate(xTask1Code, "Task1", 100, NULL, tskIDLE_PRIORITY + 1, NULL);
+    xTaskCreate(xTask2Code, "Task2", 100, NULL, tskIDLE_PRIORITY + 1, NULL);
     xTaskCreate(xUartDMA, "UartDMA", 100, NULL, tskIDLE_PRIORITY + 1, NULL);
 
     vTaskStartScheduler();
