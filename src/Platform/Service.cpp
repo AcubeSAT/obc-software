f8e083efdd3e6467fd5e097d312fe30b6c4c88c7
Antonis Keremidis <tkeremidis@gmail.com>
2022-02-06 19:52:20 +0200

    Use etl::string instead of std::ostringstream

--- src/Platform/Service.cpp
+++ src/Platform/Service.cpp
@@ -1,25 +1,29 @@
-#include <iostream>
-#include <iomanip>
+#include <etl/String.hpp>
 #include <Logger.hpp>
 #include "Service.hpp"
 
 void Service::storeMessage(Message &message) {
     // appends the remaining bits to complete a byte
     message.finalize();
+    etl::format_spec formatSpec;
+//    @todo check etl::string<size> for all strings
+    etl::string<2> serviceType;
+    etl::string<2> messageType;
+    etl::to_string(message.serviceType, serviceType, formatSpec, false);
+    etl::to_string(message.messageType, messageType, formatSpec, false);
+    etl::string<512> output = "New ";
+    (message.packetType == Message::TM) ? output.append("TM[") : output.append("TC[");
 
-    // Create a new stream to display the packet
-    std::ostringstream ss;
+    output.append(serviceType.c_str());
+    output.append(",");
+    output.append(messageType.c_str());
+    output.append("] message! ");
 
-    // Just print it to the screen
-    ss << "New " << ((message.packetType == Message::TM) ? "TM" : "TC") << "["
-       << std::hex
-       << static_cast<int>(message.serviceType) << "," // Ignore-MISRA
-       << static_cast<int>(message.messageType) // Ignore-MISRA
-       << "] message! ";
-
+    etl::string<512> data;
     for (unsigned int i = 0; i < message.dataSize; i++) {
-        ss << static_cast<int>(message.data[i]) << " "; // Ignore-MISRA
+        etl::to_string(static_cast<int>(message.data[i]), data, formatSpec, true);
+        data.append(" ");
     }
-
-    LOG_DEBUG << ss.str();
+    output.append(data);
+    LOG_DEBUG << output.c_str();
 }
