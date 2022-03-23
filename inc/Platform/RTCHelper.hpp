b1f3574e0edd6d3150b140abfab2e5682a5effb9
JulioLu <lulixhulio@gmail.com>
2022-03-23 14:43:47 +0200

    PlatformParameters Time setter

--- inc/Platform/RTCHelper.hpp
+++ inc/Platform/RTCHelper.hpp
@@ -3,13 +3,25 @@
 
 #include "PlatformParameters.hpp"
 
+const uint16_t yearBase = 1900;
+
 void setEpoch(struct tm &dateTime) {
     dateTime.tm_sec = PlatformParameters::onBoardSecond.getValue();
     dateTime.tm_min = PlatformParameters::onBoardMinute.getValue();
     dateTime.tm_hour = PlatformParameters::onBoardHour.getValue();
     dateTime.tm_mon = PlatformParameters::onBoardMonth.getValue() - 1;
     dateTime.tm_mday = PlatformParameters::onBoardDay.getValue();
-    dateTime.tm_year = PlatformParameters::onBoardYear.getValue() - 1900;
+    dateTime.tm_year = PlatformParameters::onBoardYear.getValue() - yearBase;
 }
+
+void setTimePlatformParameters(struct tm &dateTime) {
+    PlatformParameters::onBoardSecond.setValue(dateTime.tm_sec);
+    PlatformParameters::onBoardMinute.setValue(dateTime.tm_min);
+    PlatformParameters::onBoardHour.setValue(dateTime.tm_hour);
+    PlatformParameters::onBoardDay.setValue(dateTime.tm_mday);
+    PlatformParameters::onBoardMonth.setValue(dateTime.tm_mon + 1);
+    PlatformParameters::onBoardYear.setValue(dateTime.tm_year + yearBase);
+}
+
 
 #endif //OBC_SOFTWARE_RTCHELPER_HPP
