b0c2d4165c168d43a29622bac47e2cef0e482408
Antonis Keremidis <tkeremidis@gmail.com>
2022-05-29 23:18:23 +0300

    Add basic functions

--- inc/Platform/CANApplication.hpp
+++ inc/Platform/CANApplication.hpp
@@ -3,16 +3,31 @@
 
 #include <cstdint>
 
-class CANApplication{
-    static inline uint32_t addHeartbeatNodeID(uint32_t nodeID){
+class CANApplication {
+    enum BusID : uint8_t {
+        CAN_MAIN_BUS = 0,
+        CAN_REDUNDANT_BUS = 1
+    };
+
+    static inline BusID currentBus = CAN_MAIN_BUS;
+
+    static const uint8_t pingDataPacket = 0x30;
+
+    static const uint8_t pongDataPacket = 0x31;
+
+    static inline uint32_t getHeartbeatID(const uint32_t nodeID) {
         return nodeID + 0x700;
     }
 
-    static inline uint32_t addBusSwitchID(uint32_t nodeID){
+    static inline uint32_t getBusSwitchoverID(const uint32_t nodeID) {
         return nodeID + 0x400;
     }
 
-    static inline uint32_t addTimeNodeID(uint32_t nodeID){
+    static inline uint8_t getBusSwitchoverData(){
+        return currentBus ? 1 : 0;
+    }
+
+    static inline uint32_t getTimeID(const uint32_t nodeID) {
         return nodeID + 0x200;
     }
 
