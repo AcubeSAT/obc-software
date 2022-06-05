e0b8873ccb5c454af2136026e26464543670279b
JulioLu <lulixhulio@gmail.com>
2022-06-05 20:01:59 +0300

    make the task wait until the next activity

--- src/FreeRTOSTasks/TimeBasedSchedulingTask.cpp
+++ src/FreeRTOSTasks/TimeBasedSchedulingTask.cpp
@@ -2,41 +2,45 @@
 
 void TimeBasedSchedulingTask::execute() {
     auto &timeBasedScheduling = Services.timeBasedScheduling;
-    Message TCMessage(TimeBasedSchedulingService::ServiceType,TimeBasedSchedulingService::MessageType::InsertActivities, Message::TC, 1);
+    Message TCMessage(TimeBasedSchedulingService::ServiceType,
+                      TimeBasedSchedulingService::MessageType::InsertActivities, Message::TC, 1);
     TCMessage.appendUint16(4);
-    int i=0;
+    int i = 0;
     for (auto &message: TimeBasedActivities::Activities) {
         message.serviceType = 17;
         message.messageType = 1;
         message.packetType = Message::TC;
-        message.applicationId = 0;
-        message.appendUint16(4864);
-//        TCMessage.appendUint32(100+i);
-        Time::CustomCUC_t currentTime;
-        currentTime.elapsed100msTicks = xTaskGetTickCount()+10+i;
-        TCMessage.appendCustomCUCTimeStamp(currentTime);
+        Time::CustomCUC_t exe;
+        Time::CustomCUC_t now = TimeGetter::getCurrentTimeCustomCUC();
+        exe = now + i;
+        TCMessage.append(exe);
         TCMessage.appendMessage(message, ECSSTCRequestStringSize);
-        i+=10;
+        i += 1000;
     }
 
     timeBasedScheduling.insertActivities(TCMessage);
-   //Message receivedMessage1(TimeBasedSchedulingService::ServiceType, TimeBasedSchedulingService::MessageType::EnableTimeBasedScheduleExecutionFunction, Message::TC, 1);
-   //timeBasedScheduling.enableScheduleExecution(receivedMessage1);
-   //MessageParser::execute(receivedMessage1);
-    TCMessage.readPosition=0;
-//  TickType_t xLastWakeTime = xTaskGetTickCount();
+    TCMessage.readPosition = 0;
 
-    auto activities = timeBasedScheduling.getScheduledActivities(timeBasedScheduling);
+    Message receivedMessage1(TimeBasedSchedulingService::ServiceType,
+                             TimeBasedSchedulingService::MessageType::EnableTimeBasedScheduleExecutionFunction,
+                             Message::TC, 1);
+    timeBasedScheduling.enableScheduleExecution(receivedMessage1);
+
+    TickType_t xLastWakeTime = xTaskGetTickCount();
+
+    int j = 0;
     while (true) {
+        auto activity = timeBasedScheduling.getScheduledActivities();
 
-        MessageParser::execute(TimeBasedActivities::Activities[0]);
-        //MessageParser::execute(activities.at(0)->request);
-       // vTaskDelayUntil(&xLastWakeTime, pdMS_TO_TICKS(nextactivity));
+        Time::CustomCUC_t now = TimeGetter::getCurrentTimeCustomCUC();
+        TickType_t nextActivity = activity.requestReleaseTime.elapsed100msTicks - now.elapsed100msTicks;
+        vTaskDelayUntil(&xLastWakeTime, pdMS_TO_TICKS(nextActivity * 10));
+
+        MessageParser::execute(activity.request);
 
 //        Message receivedMessage(TimeBasedSchedulingService::ServiceType,
 //                                TimeBasedSchedulingService::MessageType::DetailReportAllScheduledActivities,
 //                                Message::TC, 1);
 //        timeBasedScheduling.detailReportAllActivities(receivedMessage);
-        vTaskDelay(pdMS_TO_TICKS(500));
     }
 }
