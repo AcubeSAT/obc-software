823aa9206af50c1dff064ef70b950a4825bdf7d9
Antonis Keremidis <tkeremidis@gmail.com>
2022-06-14 22:00:27 +0300

    Reduce size of IDs used

--- inc/Platform/CANMessage.hpp
+++ inc/Platform/CANMessage.hpp
@@ -10,11 +10,11 @@ private:
 
     const uint8_t pongDataPacket = 0x31;
 
-    inline uint32_t getHeartbeatID(const uint16_t nodeID) {
+    inline uint16_t getHeartbeatID(const uint16_t nodeID) {
         return nodeID + 0x700;
     }
 
-    inline uint32_t getBusSwitchoverID(const uint16_t nodeID) {
+    inline uint16_t getBusSwitchoverID(const uint16_t nodeID) {
         return nodeID + 0x400;
     }
 
@@ -22,12 +22,12 @@ private:
         return CAN::currentBus ? 0 : 1;
     }
 
-    inline uint32_t getTimeID(const uint16_t nodeID) {
+    inline uint16_t getTimeID(const uint16_t nodeID) {
         return nodeID + 0x200;
     }
 
-    inline bool isTPMessage(const uint16_t canID) {
-        return (((canID >> 7) == 0b0111));
+    inline bool isTPMessage(const uint16_t id) {
+        return (((id >> 7) == 0b0111));
     }
 public:
     struct MessageData{
