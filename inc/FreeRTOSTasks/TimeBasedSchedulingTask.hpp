9ebf9e4b9545177a6dc0ca727d951425ba4336d5
JulioLu <lulixhulio@gmail.com>
2022-06-15 20:13:52 +0300

    remove delayMs

--- inc/FreeRTOSTasks/TimeBasedSchedulingTask.hpp
+++ inc/FreeRTOSTasks/TimeBasedSchedulingTask.hpp
@@ -2,11 +2,8 @@
 #define ECSS_SERVICES_TimeBasedSchedulingTask_HPP
 
 #include "Task.hpp"
-#include "TimeBasedSchedulingService.hpp"
 
 class TimeBasedSchedulingTask : public Task {
-private:
-    const uint16_t delayMs = 1000;
 public:
     void execute();
 
