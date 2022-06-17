55771942ee9ecd9ba9202b0ff3d554a486734641
kongr45gpen <electrovesta@gmail.com>
2022-06-17 19:23:56 +0000

    Apply 1 suggestion(s) to 1 file(s)

--- inc/FreeRTOSTasks/UARTGatekeeper.hpp
+++ inc/FreeRTOSTasks/UARTGatekeeper.hpp
@@ -20,11 +20,11 @@ public:
     UARTGatekeeper();
 
     /**
+     * Adds an etl::string to the UART Gatekeeper's queue.
+     * 
      * This function was added as an extra abstraction layer to house the `xQueueSendToBack` function.
      * It can be used from anywhere in the code to get access to the UART queue/UART Gatekeeper task, without having to
      * know the low level details of the queue.
-     *
-     * Adds an etl::string to the UART Gatekeeper's queue.
      *
      * If the queue is full, the string is not added to the queue and is lost.
      * @param message the etl::string to be added in the queue of the UART Gatekeeper task.
