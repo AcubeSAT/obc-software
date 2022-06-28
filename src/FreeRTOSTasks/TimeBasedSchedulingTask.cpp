f9207374663d8b994edcb879f8323ad5c6e18311
JulioLu <lulixhulio@gmail.com>
2022-06-28 18:21:36 +0300

    execution activities by requestReleaseTime

--- src/FreeRTOSTasks/TimeBasedSchedulingTask.cpp
+++ src/FreeRTOSTasks/TimeBasedSchedulingTask.cpp
@@ -10,13 +10,15 @@ void TimeBasedSchedulingTask::execute() {
                       TimeBasedSchedulingService::MessageType::InsertActivities, Message::TC, 1);
     TCMessage.appendUint16(7);
 
+    uint16_t i = 0;
     for (auto &message: activities) {
         message.serviceType = 17;
         message.messageType = 1;
         message.packetType = Message::TC;
         Time::CustomCUC_t exe;
         Time::CustomCUC_t now = TimeGetter::getCurrentTimeCustomCUC();
-        exe = now + 80;
+        exe = now + 80 + i;
+        i += 4;
         TCMessage.append(exe);
         TCMessage.appendMessage(message, ECSSTCRequestStringSize);
     }
@@ -25,18 +27,15 @@ void TimeBasedSchedulingTask::execute() {
 
     TickType_t xLastWakeTime = xTaskGetTickCount();
 
+    Message receivedMessage(TimeBasedSchedulingService::ServiceType,
+                            TimeBasedSchedulingService::MessageType::DetailReportAllScheduledActivities,
+                            Message::TC, 1);
     while (true) {
-        auto activity = timeBasedScheduling.popScheduledActivity();
-
         Time::CustomCUC_t now = TimeGetter::getCurrentTimeCustomCUC();
-        TickType_t nextActivityExecutionTime = Time::getMsFromCustomCUC(activity.requestReleaseTime - now);
+        auto activity = timeBasedScheduling.popScheduledActivity(now);
+        TickType_t nextActivityExecutionTime = (activity - now).getMs();
         vTaskDelayUntil(&xLastWakeTime, pdMS_TO_TICKS(nextActivityExecutionTime));
 
-        MessageParser::execute(activity.request);
-
-        Message receivedMessage(TimeBasedSchedulingService::ServiceType,
-                                TimeBasedSchedulingService::MessageType::DetailReportAllScheduledActivities,
-                                Message::TC, 1);
         MessageParser::execute(receivedMessage);
     }
 }
