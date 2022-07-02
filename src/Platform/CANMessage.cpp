98fe44fa374b80ebc855174ba0bb4758e06b82c5
Antonis Keremidis <tkeremidis@gmail.com>
2022-07-02 15:52:25 +0300

    Simplify return statements for functions

--- src/Platform/CANMessage.cpp
+++ src/Platform/CANMessage.cpp
@@ -2,37 +2,18 @@
 
 using MessageData = CANMessage::MessageData;
 
-MessageData CANMessage::createPingMessage(){
-    MessageData messageData;
-
-    messageData.id = CAN::nodeID;
-    messageData.data[0] = pingDataPacket;
-
-    return messageData;
+MessageData CANMessage::createPingMessage() {
+    return {CAN::nodeID, pingDataPacket};
 }
 
-MessageData CANMessage::createPongMessage(){
-    MessageData messageData;
-
-    messageData.id = CAN::nodeID;
-    messageData.data[0] = pongDataPacket;
-
-    return messageData;
+MessageData CANMessage::createPongMessage() {
+    return {CAN::nodeID, pongDataPacket};
 }
 
-MessageData CANMessage::createHeartbeatMessage(){
-    MessageData messageData;
-
-    messageData.id = getHeartbeatID(CAN::nodeID);
-
-    return messageData;
+MessageData CANMessage::createHeartbeatMessage() {
+    return {getHeartbeatID(CAN::nodeID)};
 }
 
-MessageData CANMessage::createBusSwitchoverMessage(){
-    MessageData messageData;
-
-    messageData.id = getBusSwitchoverID(CAN::nodeID);
-    messageData.data[0] = getBusToSwitchover();
-
-    return messageData;
+MessageData CANMessage::createBusSwitchoverMessage() {
+    return {getBusSwitchoverID(CAN::nodeID), getBusToSwitchover()};
 }
