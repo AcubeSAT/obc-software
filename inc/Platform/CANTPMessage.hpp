5f707a66ac9478a39ceb233b5e8abbb578b2b680
Antonis Keremidis <tkeremidis@gmail.com>
2022-07-15 22:17:25 +0300

    Optimizations for function parameters that won't be changed

--- inc/Platform/CANTPMessage.hpp
+++ inc/Platform/CANTPMessage.hpp
@@ -16,10 +16,7 @@ namespace CANTPMessage {
     };
 
     enum EventReportType : uint8_t {
-        Informative = 0x0,
-        LowSeverity = 0x1,
-        MediumSeverity = 0x2,
-        HighSeverity = 0x3
+        Informative = 0x0, LowSeverity = 0x1, MediumSeverity = 0x2, HighSeverity = 0x3
     };
 
     IdInfo decodeId(uint16_t canID);
@@ -27,27 +24,28 @@ namespace CANTPMessage {
     uint16_t encodeId(IdInfo idInfo);
 
     template<typename T, size_t N>
-    inline void stuffIntoVector(T value, etl::vector<uint8_t, N>& vector) {
-        for (auto i = sizeof(T); i > 0;) {
-            vector.push_back(value >> (--i * 8));
+    inline void stuffIntoVector(T value, etl::vector<uint8_t, N> &vector) {
+        for (auto i = sizeof(T) - 1; i >= 0; i--) {
+            vector.push_back(value >> (i * 8));
         }
     }
 
     template<typename T>
-    void
-    createSendParametersMessage(uint8_t destinationAddress, bool isMulticast, etl::vector<uint16_t, 10> parameterIDs);
+    void createSendParametersMessage(uint8_t destinationAddress, bool isMulticast,
+                                     const etl::vector<uint16_t, 10> &parameterIDs);
 
     void createRequestParametersMessage(uint8_t destinationAddress, bool isMulticast,
-                                        etl::vector<uint16_t, 10> parameterIDs);
+                                        const etl::vector<uint16_t, 10> &parameterIDs);
 
     template<typename T>
     void createPerformFunctionMessage(uint8_t destinationAddress, bool isMulticast, uint64_t functionId,
                                       etl::vector<uint8_t, 10> argumentIDs, etl::vector<T, 10> argumentValues);
 
-    void createLogMessage(uint8_t destinationAddress, bool isMulticast, etl::string<LOGGER_MAX_MESSAGE_SIZE> log);
+    void
+    createLogMessage(uint8_t destinationAddress, bool isMulticast, const etl::string<LOGGER_MAX_MESSAGE_SIZE> &log);
 
-    void createEventReportMessage(uint8_t destinationAddress, bool isMulticast, EventReportType type,
-                                  uint16_t eventID, etl::array<uint8_t, 256> data);
+    void createEventReportMessage(uint8_t destinationAddress, bool isMulticast, EventReportType type, uint16_t eventID,
+                                  etl::array<uint8_t, 256> data);
 };
 
 #endif //OBC_SOFTWARE_CANTPMESSAGE_H
