d0a21585de84f2d2cd898b9accf4730f7d97f69f
athatheo <athatheoc@gmail.com>
2022-07-16 11:42:54 +0200

    Formatting changes

--- inc/FreeRTOSTasks/Task.hpp
+++ inc/FreeRTOSTasks/Task.hpp
@@ -20,10 +20,12 @@ public:
     const char *taskName;
 
     StaticTask_t taskBuffer;
+
     /**
      * Handle of each FreeRTOS task.
      */
     TaskHandle_t taskHandle;
+
     /**
      * The stack depth of each FreeRTOS task, defined as the number of words the stack can hold. For example, in an
      * architecture with 4 byte stack, assigning 100 to the usStackDepth argument, will allocate 4x100=400 bytes.
--- src/main.cpp
+++ src/main.cpp
@@ -10,24 +10,6 @@
 #include "FreeRTOSTasks/Task.hpp"
 #include "FreeRTOSTasks/TaskList.hpp"
 
-static StaticTask_t timekeepingTaskBuffer;
-static StackType_t timekeepingTaskStack[configMINIMAL_STACK_SIZE * 1];
-static StaticTask_t housekeepingTaskBuffer;
-static StackType_t housekeepingTaskStack[configMINIMAL_STACK_SIZE * 2];
-static StaticTask_t mcuTemperatureTaskBuffer;
-static StackType_t mcuTemperatureTaskStack[configMINIMAL_STACK_SIZE * 1];
-static StaticTask_t ambientTemperatureTaskBuffer;
-static StackType_t ambientTemperatureTaskStack[configMINIMAL_STACK_SIZE * 2];
-static StaticTask_t updateParametersTaskBuffer;
-static StackType_t updateParametersTaskStack[configMINIMAL_STACK_SIZE * 6];
-static StaticTask_t uartGatekeeperTaskBuffer;
-static StackType_t uartGatekeeperTaskStack[configMINIMAL_STACK_SIZE * 2];
-static StaticTask_t timeBasedSchedulingTaskBuffer;
-static StackType_t timeBasedSchedulingTaskStack[configMINIMAL_STACK_SIZE * 4];
-static StaticTask_t watchdogTaskBuffer;
-static StackType_t watchdogTaskStack[configMINIMAL_STACK_SIZE * 2];
-
-
 #define IDLE_TASK_SIZE 4000
 
 #if configSUPPORT_STATIC_ALLOCATION
@@ -35,7 +17,8 @@ static StackType_t watchdogTaskStack[configMINIMAL_STACK_SIZE * 2];
 static StaticTask_t xIdleTaskTCBBuffer;
 static StackType_t xIdleStack[IDLE_TASK_SIZE];
 
-extern "C" void vApplicationGetIdleTaskMemory(StaticTask_t **ppxIdleTaskTCBBuffer, StackType_t **ppxIdleTaskStackBuffer, uint32_t *pulIdleTaskStackSize) {
+extern "C" void vApplicationGetIdleTaskMemory(StaticTask_t **ppxIdleTaskTCBBuffer, StackType_t **ppxIdleTaskStackBuffer,
+                                              uint32_t *pulIdleTaskStackSize) {
     *ppxIdleTaskTCBBuffer = &xIdleTaskTCBBuffer;
     *ppxIdleTaskStackBuffer = &xIdleStack[0];
     *pulIdleTaskStackSize = IDLE_TASK_SIZE;
@@ -65,20 +48,26 @@ extern "C" void main_cpp() {
     watchdogTask.emplace();
 
     xTaskCreateStatic(vClassTask<UARTGatekeeperTask>, uartGatekeeperTask->taskName, uartGatekeeperTask->taskStackDepth,
-                      &uartGatekeeperTask, tskIDLE_PRIORITY + 2, uartGatekeeperTask->taskStack, &uartGatekeeperTask->taskBuffer);
-    xTaskCreateStatic(vClassTask<UpdateParametersTask>, updateParametersTask->taskName, updateParametersTask->taskStackDepth,
-                      &updateParametersTask, tskIDLE_PRIORITY + 1, updateParametersTaskStack,
-                      &updateParametersTaskBuffer);
+                      &uartGatekeeperTask, tskIDLE_PRIORITY + 2, uartGatekeeperTask->taskStack,
+                      &uartGatekeeperTask->taskBuffer);
+    xTaskCreateStatic(vClassTask<UpdateParametersTask>, updateParametersTask->taskName,
+                      updateParametersTask->taskStackDepth,
+                      &updateParametersTask, tskIDLE_PRIORITY + 1, updateParametersTask->taskStack,
+                      &updateParametersTask->taskBuffer);
     xTaskCreateStatic(vClassTask<TimeKeepingTask>, timeKeepingTask->taskName, timeKeepingTask->taskStackDepth,
                       &timeKeepingTask, tskIDLE_PRIORITY + 1, timeKeepingTask->taskStack, &timeKeepingTask->taskBuffer);
     xTaskCreateStatic(vClassTask<HousekeepingTask>, housekeepingTask->taskName, housekeepingTask->taskStackDepth,
-                      &housekeepingTask, configMAX_PRIORITIES - 1, housekeepingTask->taskStack, &housekeepingTask->taskBuffer);
+                      &housekeepingTask, configMAX_PRIORITIES - 1, housekeepingTask->taskStack,
+                      &housekeepingTask->taskBuffer);
     xTaskCreateStatic(vClassTask<MCUTemperatureTask>, mcuTemperatureTask->taskName, mcuTemperatureTask->taskStackDepth,
-                      &mcuTemperatureTask, tskIDLE_PRIORITY + 2, mcuTemperatureTask->taskStack, &mcuTemperatureTask->taskBuffer);
+                      &mcuTemperatureTask, tskIDLE_PRIORITY + 2, mcuTemperatureTask->taskStack,
+                      &mcuTemperatureTask->taskBuffer);
 //    xTaskCreateStatic(vClassTask<AmbientTemperatureTask>, ambientTemperatureTask->taskName, ambientTemperatureTask->taskStackDepth,
 //                &ambientTemperatureTask, tskIDLE_PRIORITY + 2, ambientTemperatureTask->taskStack, &ambientTemperatureTask->taskBuffer );
-    xTaskCreateStatic(vClassTask<TimeBasedSchedulingTask>, timeBasedSchedulingTask->taskName,timeBasedSchedulingTask->taskStackDepth,
-                      &timeBasedSchedulingTask, tskIDLE_PRIORITY + 2, timeBasedSchedulingTask->taskStack, &timeBasedSchedulingTask->taskBuffer);
+    xTaskCreateStatic(vClassTask<TimeBasedSchedulingTask>, timeBasedSchedulingTask->taskName,
+                      timeBasedSchedulingTask->taskStackDepth,
+                      &timeBasedSchedulingTask, tskIDLE_PRIORITY + 2, timeBasedSchedulingTask->taskStack,
+                      &timeBasedSchedulingTask->taskBuffer);
     xTaskCreateStatic(vClassTask<WatchdogTask>, watchdogTask->taskName, watchdogTask->taskStackDepth,
                       &watchdogTask, tskIDLE_PRIORITY, watchdogTask->taskStack, &watchdogTask->taskBuffer);
 
