3568754640d9acfd879592f1e3ad56971c7d3650
Antonis Keremidis <tkeremidis@gmail.com>
2022-07-21 18:53:50 +0300

    Implement CAN Driver

---
+++ inc/Platform/CANDriver.hpp
@@ -0,0 +1,75 @@
+#ifndef OBC_SOFTWARE_CANDRIVER_H
+#define OBC_SOFTWARE_CANDRIVER_H
+
+#include <cstdint>
+#include "ECSS_Definitions.hpp"
+#include "peripheral/mcan/plib_mcan1.h"
+#include <newlib.h>
+
+class CANDriver{
+public:
+    static inline uint8_t mcan1MessageRAM[MCAN1_MESSAGE_RAM_CONFIG_SIZE] __attribute__((aligned (32)));
+
+    /**
+     * Array that stores a message that is buffered to be sent
+     */
+    static inline MCAN_TX_BUFFER txFifo;
+
+    /**
+     * Array that stores a received message that is buffered to be processed.
+     */
+    static inline MCAN_RX_BUFFER rxFifo0;
+
+    /**
+     * Possible states for the peripheral, to be used as arguments in the callback functions.
+     */
+    typedef enum {
+        MCANReceive, MCANTransmit,
+    } APPStates;
+
+    /**
+     * This function logs a successful CAN Bus transmission.
+     * It is registered as a callback to be automatically called by Microchip's HAL whenever
+     * there is a message transmission on TX FIFO.
+     *
+     * @param context The state of the peripheral when the function is called.
+     */
+    static void txFifoCallback(uintptr_t context);
+
+    /**
+     * This function initiates a message receipt from the peripheral to the processor.
+     * It is registered as a callback to be automatically called by Microchip's HAL whenever
+     * there is a message receipt on RX FIFO 0.
+     *
+     * @param numberOfMessages The number of messages to be received from the peripheral
+     * @param context The state of the peripheral when the function is called.
+     */
+    static void rxFifo0Callback(uint8_t numberOfMessages, uintptr_t context);
+
+    /**
+     * This function logs messages that are in the Rx buffer
+     * @param numberOfMessages Number of messages to be logged
+     * @param rxBuf The RX Buffer that the messages are stored
+     * @param rxBufLen The length of the above buffer
+     */
+    static void printMessage(uint8_t numberOfMessages, MCAN_RX_BUFFER rxBuf);
+
+    /**
+     * This function decodes the data length code to get the largest expected size of the message.
+     *
+     * @param dlc The data length code
+     * @return The length of the message in bytes
+     */
+    static uint8_t convertDlcToLength(uint8_t dlc);
+
+    /**
+     * This function encodes the body of the message to the smallest data length code that it fits in.
+     *
+     * @param length The length of the message in bytes
+     * @return The data length code
+     */
+    static uint8_t convertLengthToDlc(uint8_t length);
+
+};
+
+#endif //OBC_SOFTWARE_CANDRIVER_H
---
+++ src/Platform/CANDriver.cpp
@@ -0,0 +1,68 @@
+#include "CANDriver.hpp"
+#include "Logger.hpp"
+
+uint8_t CANDriver::convertDlcToLength(uint8_t dlc) {
+    static constexpr uint8_t msgLength[] = {0U, 1U, 2U, 3U, 4U, 5U, 6U, 7U, 8U, 12U, 16U, 20U, 24U, 32U, 48U, 64U};
+    return msgLength[dlc];
+}
+
+uint8_t CANDriver::convertLengthToDlc(uint8_t length) {
+    uint8_t dlc;
+
+    if (length <= 8U) {
+        dlc = length;
+    } else if (length <= 12U) {
+        dlc = 0x9U;
+    } else if (length <= 16U) {
+        dlc = 0xAU;
+    } else if (length <= 20U) {
+        dlc = 0xBU;
+    } else if (length <= 24U) {
+        dlc = 0xCU;
+    } else if (length <= 32U) {
+        dlc = 0xDU;
+    } else if (length <= 48U) {
+        dlc = 0xEU;
+    } else {
+        dlc = 0xFU;
+    }
+    return dlc;
+}
+
+void CANDriver::txFifoCallback(uintptr_t context) {
+    uint32_t status = MCAN1_ErrorGet() & MCAN_PSR_LEC_Msk;
+
+    if (((status == MCAN_ERROR_NONE) || (status == MCAN_ERROR_LEC_NO_CHANGE)) &&
+        static_cast<APPStates>(context) == MCANTransmit) {
+//        LOG_INFO << "Successful CAN transmission\r\n";
+    } else if (static_cast<APPStates>(context) == MCANTransmit) {
+//        LOG_ERROR << "Could not transmit CAN message. The status is " << status;
+    }
+}
+
+void CANDriver::rxFifo0Callback(uint8_t numberOfMessages, uintptr_t context) {
+    uint32_t status = MCAN1_ErrorGet() & MCAN_PSR_LEC_Msk;
+
+    if (((status == MCAN_ERROR_NONE) || (status == MCAN_ERROR_LEC_NO_CHANGE)) &&
+        static_cast<APPStates>(context) == MCANReceive) {
+        memset(&rxFifo0, 0x0, MCAN1_RX_FIFO0_ELEMENT_SIZE);
+        if (MCAN1_MessageReceiveFifo(MCAN_RX_FIFO_0, numberOfMessages, &rxFifo0)) {
+            printMessage(numberOfMessages, rxFifo0);
+        }
+    }
+}
+
+void CANDriver::printMessage(uint8_t numberOfMessages, MCAN_RX_BUFFER rxBuf) {
+    for (uint8_t count = 0; count < numberOfMessages; count++) {
+        auto message = String<ECSSMaxStringSize>("\r\nCAN Message: ");
+        uint32_t id = rxBuf.id >> 18;
+        uint8_t msgLength = convertDlcToLength(rxBuf.dlc);
+        message.append("Message - ID : ");
+        etl::to_string(id, message, etl::format_spec().hex(), true);
+        message.append(" Length : ");
+        etl::to_string(msgLength, message, true);
+        message.append(" Message : ");
+        message.append(rxBuf.data, msgLength);
+        LOG_INFO << message.c_str();
+    }
+}
