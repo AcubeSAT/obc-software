eb68b1acddfe974618945479059a67bbc01087c4
Antonis Keremidis <tkeremidis@gmail.com>
2022-08-16 18:23:21 +0000

    CAN application layer

---
+++ inc/Platform/CANMessage.hpp
@@ -0,0 +1,27 @@
+#ifndef OBC_SOFTWARE_CANMESSAGE_H
+#define OBC_SOFTWARE_CANMESSAGE_H
+
+#include <algorithm>
+#include <cstdint>
+#include "etl/array.h"
+#include "OBC_Definitions.hpp"
+
+class CANMessage {
+public:
+    static constexpr uint8_t MaxDataLength = 8;
+    uint16_t id;
+    etl::array<uint8_t, MaxDataLength> data;
+
+    CANMessage() {};
+
+    CANMessage(uint16_t id) : id(id) {};
+
+    CANMessage(uint16_t id, const etl::array<uint8_t, MaxDataLength> &_data) : id(id), data(_data) {};
+
+    inline void empty() {
+        id = 0;
+        data.fill(0);
+    }
+};
+
+#endif //OBC_SOFTWARE_CANMESSAGE_H
