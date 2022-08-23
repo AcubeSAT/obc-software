771929e38a4f08e20e968800491c6eed20fb4424
ggkogkou <ggkogkou@ece.auth.gr>
2022-08-23 02:49:34 +0000

    Add All Mission Database Parameters

--- inc/Platform/Parameters/CommonParameters.hpp
+++ inc/Platform/Parameters/CommonParameters.hpp
@@ -5,15 +5,15 @@
 #include "PlatformParameters.hpp"
 
 namespace CommonParameters {
-    inline auto &boardTemperature1 = PlatformParameters::obcBoardTemperature1;
-    inline auto &mcuTemperature = PlatformParameters::mcuTemperature;
+    inline auto &boardTemperature1 = PlatformParameters::obcPCBTemperature1;
+    inline auto &mcuTemperature = PlatformParameters::obcMCUTemperature;
     inline auto &onBoardYear = PlatformParameters::onBoardYear;
     inline auto &onBoardMonth = PlatformParameters::onBoardMonth;
     inline auto &onBoardDay = PlatformParameters::onBoardDay;
     inline auto &onBoardHour = PlatformParameters::onBoardHour;
     inline auto &onBoardMinute = PlatformParameters::onBoardMinute;
     inline auto &onBoardSecond = PlatformParameters::onBoardSecond;
-    inline auto &useRTT = PlatformParameters::useRTT;
-    inline auto &useUART = PlatformParameters::useUART;
-    inline auto &useCAN = PlatformParameters::useCAN;
+    inline auto &useRTT = PlatformParameters::obcUseRTT;
+    inline auto &useUART = PlatformParameters::obcUseUART;
+    inline auto &useCAN = PlatformParameters::obcUseCAN;
 }
