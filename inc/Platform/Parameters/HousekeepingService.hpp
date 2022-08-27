4b554879ef6574097dce247857156635c1a18380
Antonis Keremidis <tkeremidis@gmail.com>
2022-08-27 13:30:26 +0300

    Add parameters from mission database to HK structures

--- inc/Platform/Parameters/HousekeepingService.hpp
+++ inc/Platform/Parameters/HousekeepingService.hpp
@@ -5,13 +5,23 @@
 #include "Services/HousekeepingService.hpp"
 
 namespace HousekeepingStructures {
-   static etl::array<HousekeepingStructure, ECSSMaxHousekeepingStructures> structures = {
-           {0, 900, true,
-            {PlatformParameters::OnBoardSecond, PlatformParameters::AvailableHeap},
-            1, 1000, true,
-            {PlatformParameters::OnBoardDay, PlatformParameters::OnBoardHour, PlatformParameters::OnBoardMinute}
-           },
-   };
+    using namespace PlatformParameters;
+
+    static etl::array<HousekeepingStructure, ECSSMaxHousekeepingStructures> structures = {
+            {
+                    {0, 1000, true, {OBCPCBTemperature1, OBCPCBTemperature2, OBCMCUTemperature, OBCMCUInputVoltage,
+                                     OBCMCUBootCounter}},
+                    {1, 1000, true, {OBCAFlashInt, OBCASRAMInt, OBCOFLASHInt, OBCOSRAMInt, OBCMRAM, OBCNAND}},
+                    {2, 1000, true,
+                     {OBCSpacecraftTimeRef, OBCMCUTime, OBCOperationalMode, OBCMemoryPartition, OBCReconfigurationTimer,
+                      OBCLastFailedEvent, OBCMCUSystick}},
+                    {3, 1000, true,
+                     {OBCCANBUSLoad1, OBCCANBUSLoad1, OBCCANBUSActive, OBCMCUFDIR, OBCMCURestartSafeModeThreshold,
+                      OBCNANDFLASHLCLThreshold, OBCMRAMLCLThreshold, OBCNANDFLASHON, OBCMRAMON}},
+                    {4, 1000, true,
+                     {OBCNANDFLASHScrubbingFrequency, OBCRAMScrubbingFrequency, OBCProgramFlashScrubbingFrequency}},
+            }
+    };
 }
 
 
