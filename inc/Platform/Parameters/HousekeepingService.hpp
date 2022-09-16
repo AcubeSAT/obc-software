2aaf37ac434b2c265e6348850d6bfb777b5ae9ce
Antonis Keremidis <tkeremidis@gmail.com>
2022-09-16 16:43:30 +0300

    Update HK Structures

--- inc/Platform/Parameters/HousekeepingService.hpp
+++ inc/Platform/Parameters/HousekeepingService.hpp
@@ -8,87 +8,108 @@ namespace HousekeepingStructures {
     using namespace PlatformParameters;
 
     static etl::array<HousekeepingStructure, ECSSMaxHousekeepingStructures> structures = {
-{
-        /**
-        * ADCS Parameters that will be monitored every 0.1 sec in the campaign
-        */
-        {0, 10, true,
-        {ADCSMagnetometerRawX,
-        ADCSMagnetometerRawY,
-        ADCSMagnetometerRawZ,
-        ADCSGyroscopeX,
-        ADCSGyroscopeY,
-        ADCSGyroscopeZ}},
-        /**
-        * OBC Mission Database parameters
-        */
-        {1, 3000, true,
-        {/* TODO: Add OnBoardTime here once it's merged */
-               ReportParametersUnusedStack,
-               AvailableHeap,
-               OBCUseCAN,
-               OBCUseRTT,
-               OBCUseUART,
-               ADCSUseCAN,
-               ADCSUseRTT,
-               ADCSUseUART,
-               OBCPCBTemperature1,
-               OBCPCBTemperature2,
-               OBCSpacecraftTimeRef,
-               OBCOperationalMode,
-               OBCReconfigurationTimer,
-               OBCLastFailedEvent,
-               OBCMCUSystick}},
-        /**
-        * ADCS Parameters that will be monitored every 3 seconds in the campaign
-        */
-        {2, 3000, true,
-        {ADCSMagnetometerFrequency,
-        ADCSMagnetometerCycleCountX,
-        ADCSMagnetometerCycleCountY,
-        ADCSMagnetometerCycleCountZ,
-        ADCSMagnetometerSelfTest,
-        ADCSGyroscopeXTemperature,
-        ADCSGyroscopeYTemperature,
-        ADCSGyroscopeZTemperature,
-        ADCSBoardTemperature1,
-        ADCSBoardTemperature2,
-        ADCSMCUTemperature,
-        ADCSBootCounter,
-        ADCSMCUOnBoardTime,
-        ADCSSystick}},
-        /**
-        * ADCS Parameters that might not be used
-        */
-        {3, 5000, false,
-        {ADCSMagnetometerSignX,
-        ADCSMagnetometerSignY,
-        ADCSMagnetometerSignZ,
-        ADCSGyroscopeX,
-        ADCSGyroscopeY,
-        ADCSGyroscopeZ,
-        ADCSGyroBiasX,
-        ADCSGyroBiasY,
-        ADCSGyroBiasZ}},
-        /**
-        * OBC Parameters that will be monitored in the campaign
-        */
-        {4, 1000, true,
-        {OBCPCBTemperature1,
-        OBCPCBTemperature2,
-        OBCMCUTemperature,
-        OBCMCUBootCounter,
-               /*OBCOnBoardTime once it's merged*/
-        OBCMemoryPartition,
-        OBCMCUSystick,
-        OBCCANBUSLoad1,
-        OBCCANBUSLoad2,
-        OBCCANBUSActive,
-        OBCNANDFLASHLCLThreshold,
-        OBCMRAMLCLThreshold,
-        OBCNANDFLASHON,
-        OBCMRAMON}}
-        }
+            {
+                    /**
+                    * OBC Parameters that will be monitored in the campaign
+                    */
+                    {0, 1000, true,
+                     {OBCPCBTemperature1,
+                      OBCPCBTemperature2,
+                      OBCMCUTemperature,
+                      OBCMCUBootCounter,
+                             /** OBCOnBoardTime once it's merged*/
+                      OBCMemoryPartition,
+                      OBCMCUSystick,
+                      OBCCANBUSLoad1,
+                      OBCCANBUSLoad2,
+                      OBCCANBUSActive,
+                      OBCNANDFLASHLCLThreshold,
+                      OBCMRAMLCLThreshold,
+                      OBCNANDFLASHON,
+                      OBCMRAMON,
+                      AvailableHeap,
+                      OBCUseCAN,
+                      OBCUseUART,
+                      OBCUseRTT,
+                      OBCBootCounter}},
+                    /**
+                    * OBC Mission Database parameters monitored every 3 sec
+                    */
+                    {1, 3000, true,
+                     {OBCPCBTemperature1,
+                      OBCPCBTemperature2,
+                      OBCSpacecraftTimeRef,
+                      OBCOperationalMode,
+                      OBCMemoryPartition,
+                      OBCReconfigurationTimer,
+                      OBCLastFailedEvent,
+                      OBCMCUSystick
+                     }},
+                    /**
+                     * OBC Mission Database parameters not sampled
+                     */
+                    {2, 5000, false,
+                     {OBCMCUInputVoltage,
+                      OBCAFlashInt,
+                      OBCASRAMInt,
+                      OBCOFLASHInt,
+                      OBCOSRAMInt,
+                      OBCMRAM,
+                      OBCNAND,
+                             /** OnBoardTime */
+                      OBCCANBUSLoad1,
+                      OBCCANBUSLoad2,
+                      OBCCANBUSActive,
+                      OBCMCUFDIR,
+                      OBCMCURestartSafeModeThreshold,
+                      OBCNANDFLASHLCLThreshold,
+                      OBCMRAMLCLThreshold,
+                      OBCNANDFLASHON,
+                      OBCNANDFLASHScrubbingFrequency,
+                      OBCRAMScrubbingFrequency,
+                      OBCProgramFlashScrubbingFrequency}},
+                    /**
+                    * ADCS Parameters that will be monitored every 0.01 sec in the campaign
+                    */
+                    {3, 10, true,
+                     {ADCSMagnetometerRawX,
+                      ADCSMagnetometerRawY,
+                      ADCSMagnetometerRawZ,
+                      ADCSGyroscopeX,
+                      ADCSGyroscopeY,
+                      ADCSGyroscopeZ}},
+                    /**
+                    * ADCS Parameters that will be monitored every 3 seconds in the campaign
+                    */
+                    {4, 3000, true,
+                     {ADCSMagnetometerFrequency,
+                      ADCSMagnetometerCycleCountX,
+                      ADCSMagnetometerCycleCountY,
+                      ADCSMagnetometerCycleCountZ,
+                      ADCSMagnetometerSelfTest,
+                      ADCSGyroscopeXTemperature,
+                      ADCSGyroscopeYTemperature,
+                      ADCSGyroscopeZTemperature,
+                      ADCSBoardTemperature1,
+                      ADCSBoardTemperature2,
+                      ADCSMCUTemperature,
+                      ADCSBootCounter,
+                      ADCSMCUOnBoardTime,
+                      ADCSSystick}},
+                    /**
+                    * ADCS Parameters that might not be used
+                    */
+                    {5, 5000, false,
+                     {ADCSMagnetometerSignX,
+                      ADCSMagnetometerSignY,
+                      ADCSMagnetometerSignZ,
+                      ADCSGyroscopeX,
+                      ADCSGyroscopeY,
+                      ADCSGyroscopeZ,
+                      ADCSGyroBiasX,
+                      ADCSGyroBiasY,
+                      ADCSGyroBiasZ}},
+            }
     };
 }
 
