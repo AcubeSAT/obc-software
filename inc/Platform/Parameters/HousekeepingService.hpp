878c40f2f9ff7a6b4a6daf16b5782fee8988a159
Antonis Keremidis <tkeremidis@gmail.com>
2022-09-26 22:43:54 +0300

    Change comment styles

--- inc/Platform/Parameters/HousekeepingService.hpp
+++ inc/Platform/Parameters/HousekeepingService.hpp
@@ -9,15 +9,13 @@ namespace HousekeepingStructures {
 
     static etl::array<HousekeepingStructure, ECSSMaxHousekeepingStructures> structures = {
             {
-                    /**
-                    * OBC Parameters that will be monitored in the campaign
-                    */
+                    // OBC Parameters that will be monitored in the campaign
                     {0, 1000, true,
                      {OBCPCBTemperature1,
                       OBCPCBTemperature2,
                       OBCMCUTemperature,
                       OBCMCUBootCounter,
-                             /** OBCOnBoardTime once it's merged*/
+                             //TODO OBCOnBoardTime once it's merged
                       OBCMemoryPartition,
                       OBCMCUSystick,
                       OBCCANBUSLoad1,
@@ -31,9 +29,7 @@ namespace HousekeepingStructures {
                       OBCUseCAN,
                       OBCUseUART,
                       OBCUseRTT}},
-                    /**
-                    * OBC Mission Database parameters monitored every 3 sec
-                    */
+                    // OBC Mission Database parameters monitored every 3 sec
                     {1, 3000, true,
                      {OBCPCBTemperature1,
                       OBCPCBTemperature2,
@@ -44,16 +40,14 @@ namespace HousekeepingStructures {
                       OBCLastFailedEvent,
                       OBCMCUSystick
                      }},
-                    /**
-                     * OBC Mission Database parameters not sampled
-                     */
+                    // OBC Mission Database parameters not sampled
                     {2, 5000, false,
                      {OBCMCUInputVoltage,
                       OBCFlashInt,
                       OBCSRAMInt,
                       OBCMRAM,
                       OBCNAND,
-                             /** OnBoardTime */
+                             //TODO OBCOnBoardTime once it's merged
                       OBCCANBUSLoad1,
                       OBCCANBUSLoad2,
                       OBCCANBUSActive,
@@ -65,9 +59,7 @@ namespace HousekeepingStructures {
                       OBCNANDFLASHScrubbingFrequency,
                       OBCRAMScrubbingFrequency,
                       OBCProgramFlashScrubbingFrequency}},
-                    /**
-                    * ADCS Parameters that will be monitored every 0.01 sec in the campaign
-                    */
+                    // ADCS Parameters that will be monitored every 0.01 sec in the campaign
                     {3, 10, true,
                      {ADCSMagnetometerRawX,
                       ADCSMagnetometerRawY,
@@ -75,9 +67,7 @@ namespace HousekeepingStructures {
                       ADCSGyroscopeX,
                       ADCSGyroscopeY,
                       ADCSGyroscopeZ}},
-                    /**
-                    * ADCS Parameters that will be monitored every 3 seconds in the campaign
-                    */
+                    // ADCS Parameters that will be monitored every 3 seconds in the campaign
                     {4, 3000, true,
                      {ADCSMagnetometerFrequency,
                       ADCSMagnetometerCycleCountX,
@@ -93,9 +83,7 @@ namespace HousekeepingStructures {
                       ADCSBootCounter,
                       ADCSMCUOnBoardTime,
                       ADCSSystick}},
-                    /**
-                    * ADCS Parameters that might not be used
-                    */
+                    // ADCS Parameters that might not be used
                     {5, 5000, false,
                      {ADCSMagnetometerSignX,
                       ADCSMagnetometerSignY,
