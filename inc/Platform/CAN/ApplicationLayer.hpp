9237bed5687ad51b0a988e9340df9957d9d119f6
Antonis Keremidis <tkeremidis@gmail.com>
2022-10-13 14:29:01 +0300

    Move things to structured application layer

---
+++ inc/Platform/CAN/ApplicationLayer.hpp
@@ -0,0 +1,85 @@
+#pragma once
+
+#include <cstdint>
+
+namespace CAN::Application {
+    /**
+     * CAN-TP message IDs, as specified in DDJF_OBDH.
+     */
+    enum MessageIDs : uint16_t {
+        SendParameters = 0x01,
+        RequestParameters = 0x02,
+        PerformFunction = 0x03,
+        EventReport = 0x04,
+        TMPacket = 0x20,
+        TCPacket = 0x21,
+        CCSDSPacket = 0x022,
+        Ping = 0x30,
+        Pong = 0x31,
+        LogMessage = 0x40,
+        UTCTime = 0x200,
+        BusSwitchover = 0x400,
+        Heartbeat = 0x700
+    };
+
+    enum ActiveBus : uint8_t {
+        MainBus = 0x0,
+        RedundantBus = 0x1
+    };
+
+    ActiveBus currentBus = MainBus;
+
+    inline uint8_t toggleBusSwitchover() {
+        if (currentBus == MainBus) {
+            currentBus = RedundantBus;
+        } else {
+            currentBus = MainBus;
+        }
+        return currentBus;
+    }
+
+    /**
+     * Adds a Ping message to the outgoing queue, according to DDJF_OBDH.
+     */
+    void sendPingMessage();
+
+    /**
+     * Adds a Pong message to the outgoing queue, to be sent in response to a Ping message, according to DDJF_OBDH.
+     */
+    void sendPongMessage();
+
+    /**
+     * Adds a Heartbeat message to the outgoing queue, to be called periodically, according to DDJF_OBDH.
+     */
+    void sendHeartbeatMessage();
+
+    /**
+     * Creates a CAN Bus switchover message to be sent whenever the FDIR responsible determines the bus quality
+     * is not satisfactory, according to DDJF_OBDH.
+     */
+    void sendBusSwitchoverMessage();
+
+    /**
+     * Adds a message to the outgoing queue with the current UTC Time, according to DDJF_OBDH.
+     */
+    void sendUTCTimeMessage();
+
+    /**
+     * Determines whether a message is following the CAN-TP Message protocol.
+     * @param id The Id of the message received via CAN
+     * @return True if the message is part of a CAN-TP Message
+     */
+    inline bool isTPMessage(uint16_t id) {
+        return (id >> 7) == 0b0111;
+    }
+
+    /**
+     * Removes the id of the sender in an incoming CAN Message.
+     * @param id The ID to be filtered.
+     * @return The filtered ID.
+     */
+    inline uint16_t filterMessageID(uint16_t id) {
+        return id & 0x700;
+    }
+
+}
---
+++ src/Platform/CAN/ApplicationLayer.cpp
@@ -0,0 +1,37 @@
+#include "CAN/ApplicationLayer.hpp"
+#include "CANGatekeeperTask.hpp"
+
+namespace CAN::Application {
+    void sendPingMessage() {
+        canGatekeeperTask->addToQueue({MessageIDs::Ping + CAN::NodeID});
+    }
+
+    void sendPongMessage() {
+        canGatekeeperTask->addToQueue({MessageIDs::Pong + CAN::NodeID});
+    }
+
+    void sendHeartbeatMessage() {
+        canGatekeeperTask->addToQueue({MessageIDs::Heartbeat + CAN::NodeID});
+    }
+
+    void sendBusSwitchoverMessage() {
+        etl::vector<uint8_t, CAN::Message::MaxDataLength> data = {toggleBusSwitchover()};
+
+        canGatekeeperTask->addToQueue({MessageIDs::BusSwitchover + CAN::NodeID, data});
+    }
+
+    void sendUTCTimeMessage() {
+        auto now = TimeGetter::getCurrentTimeDefaultCUC();
+
+        std::chrono::duration<uint64_t, std::milli> msOfDay = now.asDuration(); //@todo This doesn't reset every day, only since epoch.
+
+        UTCTimestamp utc = now.toUTCtimestamp();
+        etl::vector<uint8_t, CAN::Message::MaxDataLength> data = {0, 0, static_cast<uint8_t>(msOfDay.count()),
+                                                                  static_cast<uint8_t>(msOfDay.count() >> 8),
+                                                                  static_cast<uint8_t>(msOfDay.count() >> 16),
+                                                                  static_cast<uint8_t>(msOfDay.count() >> 24), 0,
+                                                                  utc.day}; //@todo days parameter should not be uint8_t
+
+        canGatekeeperTask->addToQueue({MessageIDs::UTCTime + CAN::NodeID, data});
+    }
+}
