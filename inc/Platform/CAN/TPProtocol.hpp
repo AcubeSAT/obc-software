24ecc4ab633bc32a18fe5eb0abe59e455080632d
Antonis Keremidis <tkeremidis@gmail.com>
2022-10-14 20:52:25 +0300

    Move more things into namespaced TPProtocol

---
+++ inc/Platform/CAN/TPProtocol.hpp
@@ -0,0 +1,68 @@
+#pragma once
+
+#include "CAN/ApplicationLayer.hpp"
+#include "CAN/Packet.hpp"
+#include "CAN/TPMessage.hpp"
+
+namespace CAN::TPProtocol {
+    /**
+     * Types of CAN-TP protocol frames.
+     */
+    enum Frame : uint8_t {
+        Single = 0x00, First = 0x01, Consecutive = 0x02, FlowControl = 0x03
+    };
+
+    /**
+     * How many bytes of information are contained in a consecutive frame
+     */
+    static constexpr uint8_t BytesInConsecutiveFrame = 6;
+
+    /**
+     * A pointer indicating the information starting point.
+     */
+    static constexpr uint8_t BytesStartingPoint = 2;
+
+    //TODO Should this be a single message in the namespace? Handle processing of many messages.
+    static TPMessage tpMessage;
+
+    /**
+     * A structure holding the data length codes of the received CAN-TP messages.
+     * TODO This doesn't make sense being a vector of size 1.
+     */
+    inline etl::vector<uint16_t, 1> dataLengthCodes;
+
+    /**
+     * Receives CAN-TP Protocol message frame, processes its bytes for information and then saves it to the incoming
+     * messages map.
+     * @param messageFrame the received CAN-TP frame
+     */
+    void saveCANTPMessage(const CAN::Packet &messageFrame);
+
+    /**
+     * Extracts information(Map key, data length code) from the first frame
+     * @param messageFrame the received CAN-TP Frame
+     * //TODO Docs mention a CAN-TP Frame, should this be a single 8-byte CAN::Packet?
+     * @return the map key and the message data length code
+     */
+    inline uint16_t extractDataLengthCode(const CAN::Packet &messageFrame) {
+        uint8_t dataLengthCodeLSB = messageFrame.data[0] << 4;
+        uint8_t dataLengthCodeMSB = messageFrame.data[1];
+
+        return (static_cast<uint16_t>(dataLengthCodeMSB) << 8) | dataLengthCodeLSB;
+    }
+
+    /**
+     * Processes the stored messages received and acts on their content accordingly.
+     * @param message the complete CAN-TP message.
+     */
+    void parseMessage(const TPMessage &message);
+
+    /**
+     * Splits a CAN-TP Message into a collection of CAN-TP frames and adds them to the CAN Gatekeeper Task queue.
+     * @param id the CAN Packet encoded id
+     * @param messageMapKey a map key to save the message correctly to the message map when received
+     * @param messagePayload one of the CAN-TP messages found in DDJF_OBDH
+     */
+    void createCANTPMessage(uint16_t id, const etl::vector<uint8_t, CAN::TPMessageMaximumSize> &messagePayload,
+                            uint16_t messageSize);
+}
---
+++ src/Platform/CAN/TPProtocol.cpp
@@ -0,0 +1,83 @@
+#include "CAN/TPProtocol.hpp"
+#include "CANGatekeeperTask.hpp"
+
+namespace CAN {
+    void TPProtocol::saveCANTPMessage(const CAN::Packet &messageFrame) {
+        uint8_t frameType = messageFrame.data[0] >> 4;
+        if (frameType == First) {
+            dataLengthCodes[0] = (extractDataLengthCode(messageFrame));
+
+        } else if (frameType == Consecutive) {
+            for (uint8_t i = 1; i < BytesInConsecutiveFrame; i++) {
+                tpMessage.appendUint8(messageFrame.data[i]);
+            }
+
+            if (tpMessage.dataSize == dataLengthCodes[0]) {
+                parseMessage(tpMessage);
+            }
+        }
+    }
+
+    void TPProtocol::parseMessage(const TPMessage &message) {
+        uint8_t frameType; //todo Get Frame type
+        switch (frameType) {
+            case CAN::Application::SendParameters:
+                break; //todo: use ParameterService to update the values.
+            case CAN::Application::RequestParameters:
+                break; //todo: send back the requested parameters.
+            case CAN::Application::PerformFunction:
+                break; //todo: using ST[08] execute the perform function command
+            case CAN::Application::EventReport:
+                break; //todo: use the Event Report service to... report the event.... hehe
+            case CAN::Application::TMPacket:
+                break; //todo: idk
+            case CAN::Application::TCPacket:
+                break; //todo execute the incoming TC
+            case CAN::Application::CCSDSPacket:
+                break; //todo send this to comms? idk
+            case CAN::Application::Ping:
+                CAN::Application::sendPongMessage();
+            case CAN::Application::Pong:
+                break; //todo Saul goodman
+            case CAN::Application::LogMessage:
+                break; //todo LOG this message.
+            default:
+                break; //todo Error handling
+        }
+    }
+
+    void
+    TPProtocol::createCANTPMessage(uint16_t id, const etl::vector<uint8_t, CAN::TPMessageMaximumSize> &messagePayload,
+                                   uint16_t messageSize) {
+        // 4 MSB bits is the frame type id and the 4 LSB are the 4 out of 12 bits for the data length code.
+        uint8_t idDLC = (First << 4) | (messageSize >> 4);
+        // Rest 8 bits of data length code.
+        uint8_t DLC = messageSize << 4;
+
+        etl::vector<uint8_t, CAN::Packet::MaxDataLength> firstFrame = {idDLC, DLC};
+
+        canGatekeeperTask->addToQueue({id, firstFrame});
+
+        //Start creating the consecutive frames.
+        uint8_t currentConsecutiveFrameCount = 0x01;
+        uint8_t consecutiveFrameElements = (Consecutive << 4) | currentConsecutiveFrameCount;
+        etl::vector<uint8_t, CAN::Packet::MaxDataLength> consecutiveFrame = {consecutiveFrameElements};
+        uint16_t byteCounter = 0;
+
+        for (uint16_t idx = 1; idx < messageSize; idx++) {
+            if (byteCounter % CAN::Packet::MaxDataLength == 0) {
+                byteCounter = 1;
+
+                canGatekeeperTask->addToQueue({id, consecutiveFrame});
+                currentConsecutiveFrameCount++;
+
+                consecutiveFrameElements = (Consecutive << 4) | currentConsecutiveFrameCount;
+                consecutiveFrame = {consecutiveFrameElements};
+                if (currentConsecutiveFrameCount == 0x0F) {
+                    currentConsecutiveFrameCount = 0x00;
+                }
+            }
+            consecutiveFrame[byteCounter++] = messagePayload[idx];
+        }
+    }
+}
