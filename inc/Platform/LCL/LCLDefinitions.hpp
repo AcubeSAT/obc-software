23044cfd796a258dfe7c5478979433d7095584e3
Stelios Tzelepis <stel.tze09@gmail.com>
2022-11-09 23:05:10 +0200

    Complete LCL definitions

--- inc/Platform/LCL/LCLDefinitions.hpp
+++ inc/Platform/LCL/LCLDefinitions.hpp
@@ -1,72 +1,52 @@
 #pragma once
 
 #include "LCL.hpp"
-#include "peripheral/pio/plib_pio.h"
 
 namespace LCLDefinitions {
 
-//    /**
-//     * @enum PWM channels used to set the current threshold for the TLC555 timer.
-//     * PWM0 of ATSAMV71 is used for all thresholds.
-//     */
-//    enum class PWMChannelMasks : uint8_t {
-//        NANDFlash = PWM_CHANNEL_0_MASK, ///< NAND Flash
-//        MRAM = PWM_CHANNEL_1_MASK, ///< MRAM
-//        CAN1 = PWM_CHANNEL_3_MASK, ///< CAN1
-//        CAN2 = PWM_CHANNEL_2_MASK ///< CAN2
-//    };
-//
-//    /**
-//     * @enum PWM channels used to set the current threshold for the TLC555 timer.
-//     */
-//    enum class PWMChannelNumbers : uint8_t {
-//        NANDFlash = PWM_CHANNEL_0, ///< NAND Flash
-//        MRAM = PWM_CHANNEL_1, ///< MRAM
-//        CAN1 = PWM_CHANNEL_3, ///< CAN1
-//        CAN2 = PWM_CHANNEL_2 ///< CAN2
-//    };
-//
-//    /**
-//     * @enum GPIOs used reset the LCL, specifically the TLC555 IC.
-//     * Active low.
-//     * Initial value given by a pull-up resistor to prevent undefined behavior during MCU
-//     * reset/start-up.
-//     */
-//    enum class ResetPins : uint8_t {
-//        NANDFlash = PIO_PIN_PC15, ///< NAND Flash, LCL_CAN_1_RST_PIN plib_pio.h
-//        MRAM = PIO_PIN_PE4, ///< MRAM
-//        CAN1 = PIO_PIN_PD24, ///< CAN1
-//        CAN2 = PIO_PIN_PA26 ///< CAN2
-//    };
-//
-//    /**
-//     * @enum GPIOs used reset the LCL, specifically the TLC555 IC.
-//     * Active low.
-//     * Initial value given by a pull-up resistor to prevent undefined behavior during MCU
-//     * reset/start-up.
-//     */
-//    enum class SetPins : uint8_t {
-//        NANDFlash = PIO_PIN_PC13, ///< MRAM
-//        MRAM = PIO_PIN_PA17, ///< NAND Flash
-//        CAN1 = PIO_PIN_PD26, ///< CAN1
-//        CAN2 = PIO_PIN_PD22 ///< CAN2
-//    };
-//
-//
-//    /**
-//     * @enum Devices protected by LCLs
-//     */
-//    enum class ProtectedDevices : uint8_t {
-//        NANDFlash = 0,
-//        MRAM = 1,
-//        CAN1 = 2,
-//        CAN2 = 3
-//    };
+    enum class ProtectedDevices : uint8_t {
+        NANDFlash = 0,
+        MRAM = 1,
+        CAN1 = 2,
+        CAN2 = 3,
+    };
 
-    LCLControlPins nandFlashControlPins = {LCL_NAND_RST_PIN, LCL_NAND_SET_PIN};
-    LCLControlPins mramControlPins = {LCL_MRAM_RST_PIN, LCL_MRAM_SET_PIN};
-    LCLControlPins can1ControlPins = {LCL_CAN_1_RST_PIN, LCL_CAN_1_SET_PIN};
-    LCLControlPins can2ControlPins = {LCL_CAN_2_RST_PIN, LCL_CAN_2_SET_PIN};
+    enum class PWMChannelMasks : PWM_CHANNEL_MASK {
+        NANDFlash = PWM_CHANNEL_0_MASK,
+        MRAM = PWM_CHANNEL_1_MASK,
+        CAN1 = PWM_CHANNEL_3_MASK,
+        CAN2 = PWM_CHANNEL_2_MASK
+    };
+
+    enum class PWMChannelNumbers : PWM_CHANNEL_NUM {
+        NANDFlash = PWM_CHANNEL_0,
+        MRAM = PWM_CHANNEL_1,
+        CAN1 = PWM_CHANNEL_3,
+        CAN2 = PWM_CHANNEL_2
+    };
+
+    enum class ResetPins : uint32_t {
+        NANDFlash = LCL_NAND_RST_PIN,
+        MRAM = LCL_MRAM_RST_PIN,
+        CAN1 = LCL_CAN_1_RST_PIN,
+        CAN2 = LCL_CAN_2_RST_PIN
+    };
+
+    enum class SetPins : uint32_t {
+        NANDFlash = LCL_NAND_SET_PIN,
+        MRAM = LCL_MRAM_SET_PIN,
+        CAN1 = LCL_CAN_1_SET_PIN,
+        CAN2 = LCL_CAN_2_SET_PIN
+    };
+
+    LCLControlPins nandFlashControlPins = {ResetPins::NANDFlash, SetPins::NANDFlash,
+                                           PWMChannelMasks::NANDFlash, PWMChannelNumbers::NANDFlash};
+    LCLControlPins mramControlPins = {ResetPins::MRAM, SetPins::MRAM, PWMChannelMasks::MRAM,
+                                      PWMChannelNumbers::MRAM};
+    LCLControlPins can1ControlPins = {ResetPins::CAN1, SetPins::CAN1, PWMChannelMasks::CAN1,
+                                      PWMChannelNumbers::CAN1};
+    LCLControlPins can2ControlPins = {ResetPins::CAN2, SetPins::CAN2, PWMChannelMasks::CAN2,
+                                      PWMChannelNumbers::CAN2};
 
     inline std::optional<LCL> nandFlashLCL(LCLDefinitions::nandFlashControlPins);
     inline std::optional<LCL> mramLCL(LCLDefinitions::mramControlPins);
