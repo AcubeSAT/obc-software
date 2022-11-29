8c1a38ffb7715b296baa95dae27f41e05ffd9799
Antonis Keremidis <tkeremidis@gmail.com>
2022-11-29 13:30:49 +0200

    Add more tests to run

--- src/FreeRTOSTasks/CANTestTask.cpp
+++ src/FreeRTOSTasks/CANTestTask.cpp
@@ -4,6 +4,9 @@
 void CANTestTask::execute() {
     CAN::Frame message = {};
 
+    /**
+     * Simple 64 byte message sending
+     */
     message.id = 0x45A;
     for (uint8_t idx = 0; idx < CAN::Frame::MaxDataLength; idx++) {
         message.data.push_back(idx);
@@ -13,4 +16,29 @@ void CANTestTask::execute() {
         canGatekeeperTask->send(message);
         vTaskDelay(pdMS_TO_TICKS(1000));
     }
+
+    /**
+     * Ping messages to OBC
+     */
+//    while (true) {
+//        CAN::Application::sendPingMessage(CAN::Application::NodeIDs::OBC, false);
+//        vTaskDelay(pdMS_TO_TICKS(1000));
+//    }
+
+    /**
+     * Housekeeping parameters sending
+     */
+//    while (true) {
+//        CAN::Application::createSendParametersMessage(CAN::Application::NodeIDs::OBC, false, {2, 7}, false);
+//        vTaskDelay(pdMS_TO_TICKS(1000));
+//    }
+
+    /**
+     * Log messages utilizing multiple messages
+     */
+//     while (true) {
+//         const String<LoggerMaxMessageSize> logString = "This is a log message going from the development board to the eqm that consists of more than 64 chars";
+//         CAN::Application::createLogMessage(CAN::Application::NodeIDs::OBC, false, logString, false);
+//         vTaskDelay(pdMS_TO_TICKS(1000));
+//     }
 }
