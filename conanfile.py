1e07303b6bc65a7f736dbd307a4fee4552145436
athatheo <athatheoc@gmail.com>
2023-06-21 21:21:14 +0300

    commit

---
+++ conanfile.py
@@ -0,0 +1,54 @@
+from os.path import join
+
+from conan import ConanFile
+from conan.tools.cmake import CMakeToolchain, CMake, cmake_layout
+from conan.tools.files import copy
+from conan.tools.scm import Git
+
+
+class OBCSWRecipe(ConanFile):
+    name = "obc-sw"
+    version = "1.0"
+    revision_mode = "scm"
+
+    # Optional metadata
+    license = "MIT"
+    author = "SpaceDot - AcubeSAT, acubesat.obc@spacedot.gr"
+    url = "gitlab.com/acubesat/obc/ecss-services"
+    description = "ECSS Services implementation for the AcubeSAT nanosatellite"
+    topics = ("satellite", "acubesat", "obc", "obc-software")
+
+    # Binary configuration
+    settings = "os", "compiler", "build_type", "arch"
+    options = {"shared": [True, False], "fPIC": [True, False]}
+    default_options = {"shared": False, "fPIC": True}
+
+    # Sources are located in the same place as this recipe, copy them to the recipe
+    exports_sources = "CMakeLists.txt", "src/*", "inc/*"
+    generators = "CMakeDeps"
+
+    def config_options(self):
+        if self.settings.os == "Windows":
+            del self.options.fPIC
+
+    def layout(self):
+        cmake_layout(self)
+
+    def generate(self):
+        tc = CMakeToolchain(self)
+        tc.generate()
+
+    def build(self):
+        git = Git(self)
+        git.clone(url="https://gitlab.com/acubesat/obc/cross-platform-software", target="lib/")
+        # Please, be aware that using the head of the branch instead of an immutable tag
+        # or commit is not a good practice in general
+        git.checkout("main")
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
+
+    def requirements(self):
+        self.requires("etl/20.32.1")
+        self.requires("logger/1.0")
+        self.requires("ecss-services/1.0")
