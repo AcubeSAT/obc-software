3745f153a5b1eedcad59838d466c561032c0818e
athatheo <athatheoc@gmail.com>
2023-06-22 12:07:26 +0000

    Update file conanfile.py

--- conanfile.py
+++ conanfile.py
@@ -43,6 +43,7 @@ class OBCSoftwareRecipe(ConanFile):
 
     def generate(self):
         tc = CMakeToolchain(self)
+        tc.variables["NO_SYSTEM_INCLUDE"] = True
         tc.generate()
 
     def build(self):
