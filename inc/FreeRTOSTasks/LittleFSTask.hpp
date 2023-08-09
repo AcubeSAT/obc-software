5d0d926615adc23098c73f09450eae53028f2a04
kchristin22 <christinakoutsou22@gmail.com>
2023-08-09 14:59:57 +0300

    Integrate LittleFS to obc-software

---
+++ inc/FreeRTOSTasks/LittleFSTask.hpp
@@ -0,0 +1,12 @@
+#ifndef OBC_SOFTWARE_LITTLEFS_HPP
+#define OBC_SOFTWARE_LITTLEFS_HPP
+
+#include "lfs.h"
+#include "Task.hpp"
+
+class LittleFSTask: public Task{
+private:
+
+};
+
+#endif //OBC_SOFTWARE_LITTLEFS_HPP
