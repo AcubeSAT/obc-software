99978de36dac8129512c26b3dfde244b36403d38
Epatsatzaki <evapatsatzakis@gmail.com>
2024-02-21 17:28:26 +0200

    format code

--- src/FreeRTOSTasks/HousekeepingTask.cpp
+++ src/FreeRTOSTasks/HousekeepingTask.cpp
@@ -3,11 +3,11 @@
 void HousekeepingTask::execute() {
     auto &housekeeping = Services.housekeeping;
     Time::DefaultCUC nextCollection(0);
-    Time::DefaultCUC timeBeforeDelay( 0);
+    Time::DefaultCUC timeBeforeDelay(0);
     TickType_t xLastWakeTime = xTaskGetTickCount();
-    Time::DefaultCUC TaskGetTickCountCUC (pdTICKS_TO_S(xTaskGetTickCount()));
+    Time::DefaultCUC TaskGetTickCountCUC(pdTICKS_TO_S(xTaskGetTickCount()));
     while (true) {
-        nextCollection = housekeeping.reportPendingStructures(TaskGetTickCountCUC , timeBeforeDelay, nextCollection);
+        nextCollection = housekeeping.reportPendingStructures(TaskGetTickCountCUC, timeBeforeDelay, nextCollection);
         timeBeforeDelay = TaskGetTickCountCUC;
         vTaskDelayUntil(&xLastWakeTime, pdMS_TO_TICKS(nextCollection.formatAsBytes()));
     }
