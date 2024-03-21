1b676b7d9e9c814cc01306fecac11472ebbdec6a
Epatsatzaki <evapatsatzakis@gmail.com>
2024-03-21 16:04:57 +0200

    corrected the channel

--- src/FreeRTOSTasks/HeaterTask.cpp
+++ src/FreeRTOSTasks/HeaterTask.cpp
@@ -9,7 +9,7 @@ void HeaterTask::execute() {
 
     PWM0_Initialize();
 
-    PWM0_ChannelPeriodSet(PWM_CHANNEL_0, 7500);
+    PWM0_ChannelPeriodSet(PWM_CHANNEL_2, 7500);
     PWM0_ChannelDutySet(PWM_CHANNEL_2, 3750);
     PWM0_ChannelsStart(PWM_CHANNEL_2_MASK);
     vTaskDelay(pdMS_TO_TICKS(100));
