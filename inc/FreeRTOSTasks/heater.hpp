9b75bf3124d3e08a1710e1400913d1322a774ecc
Epatsatzaki <evapatsatzakis@gmail.com>
2024-04-01 21:18:54 +0300

    separation of heater file to hpp and cpp

--- inc/FreeRTOSTasks/heater.hpp
+++ inc/FreeRTOSTasks/heater.hpp
@@ -1,6 +1,6 @@
 #pragma once
 
-#include "peripheral/pwm/plib_pwm0.h"
+#include <cstdint>
 
 class heater {
 
@@ -12,35 +12,25 @@ public:
      * This initializes all the selected channels of given peripheral in MCC GUI.
      * This function must be called before any other PWM function is called.
      * This function should only be called once during system initialization.*/
-    static void initializeHeater() {
-        PWM0_Initialize();
-    }
+    static void initializeHeater();
 
     //This function disables the 2nd channel of the PWM0
-    static void startHeater() {
-        PWM0_ChannelsStart(PWM_CHANNEL_2_MASK);
-    }
+    static void startHeater();
 
     //This function enables the 2nd channel of the PWM0
-    static void stopHeater() {
-        PWM0_ChannelsStop(PWM_CHANNEL_2_MASK);
-    }
+    static void stopHeater() ;
 
     //sets the Duty of PWM
-    void setDutyPercentage(float dutyValuePercentage) {
-        PWM0_ChannelDutySet(PWM_CHANNEL_2, convertDutyCycleToMilliseconds(dutyValuePercentage));
-    }
+    void setDutyPercentage(float dutyValuePercentage);
 
     /* dutyValue() takes the duty cycle percentage of the waveform as an argument
      * and returns the on-time of the waveform in ticks.
      */
-    float convertDutyCycleToMilliseconds(float dutyCyclePercentage) {
-        return period * (dutyCyclePercentage / 100);
-    }
+    float convertDutyCycleToMilliseconds(float dutyCyclePercentage);
 
 
 private:
 
-    const float period = PWM0_ChannelPeriodGet(PWM_CHANNEL_2);   // equals to the period of the waveform in ticks
+    static float period ; // equals to the period of the waveform in ticks
 
 };
--- src/FreeRTOSTasks/heater.cpp
+++ src/FreeRTOSTasks/heater.cpp
@@ -1,1 +1,30 @@
+#include "inc/FreeRTOSTasks/heater.hpp"
+#include "peripheral/pwm/plib_pwm0.h"
 
+void heater::initializeHeater() {
+    PWM0_Initialize();
+}
+
+//This function disables the 2nd channel of the PWM0
+void heater::startHeater() {
+    PWM0_ChannelsStart(PWM_CHANNEL_2_MASK);
+}
+
+//This function enables the 2nd channel of the PWM0
+void heater::stopHeater() {
+    PWM0_ChannelsStop(PWM_CHANNEL_2_MASK);
+}
+
+//sets the Duty of PWM
+void heater::setDutyPercentage(float dutyValuePercentage) {
+    PWM0_ChannelDutySet(PWM_CHANNEL_2, convertDutyCycleToMilliseconds(dutyValuePercentage));
+}
+
+/* dutyValue() takes the duty cycle percentage of the waveform as an argument
+ * and returns the on-time of the waveform in ticks.
+ */
+float heater::convertDutyCycleToMilliseconds(float dutyCyclePercentage) {
+    return period * (dutyCyclePercentage / 100);
+}
+
+float heater::period = PWM0_ChannelPeriodGet(PWM_CHANNEL_2);   // equals to the period of the waveform in ticks
