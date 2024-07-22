912ed7f24245bf0aa8bf882be8ddd248f12ae748
athatheo <athatheoc@gmail.com>
2024-07-23 02:00:10 +0300

    Use cmkae to build

---
+++ src/main.cpp
@@ -0,0 +1,39 @@
+#include "main.h"
+#include "FreeRTOS.h"
+#include "queue.h"
+#include "list.h"
+#include "task.h"
+#include "definitions.h"
+#include "InitializationTask.hpp"
+#include "FreeRTOSHandlers.hpp"
+
+#define IDLE_TASK_SIZE 100
+
+#if configSUPPORT_STATIC_ALLOCATION
+/* static memory allocation for the IDLE task */
+static StaticTask_t xIdleTaskTCBBuffer;
+static StackType_t xIdleStack[IDLE_TASK_SIZE];
+
+extern "C" void vApplicationGetIdleTaskMemory(StaticTask_t **ppxIdleTaskTCBBuffer, StackType_t **ppxIdleTaskStackBuffer,
+                                              uint32_t *pulIdleTaskStackSize) {
+    *ppxIdleTaskTCBBuffer = &xIdleTaskTCBBuffer;
+    *ppxIdleTaskStackBuffer = &xIdleStack[0];
+    *pulIdleTaskStackSize = IDLE_TASK_SIZE;
+}
+
+#endif
+
+extern "C" void main_cpp() {
+    SYS_Initialize(NULL);
+    initializationTask.emplace();
+
+    initializationTask->createTask();
+
+
+    vTaskStartScheduler();
+
+    while (true) {
+        /* Maintain state machines of all polled MPLAB Harmony modules. */
+        SYS_Tasks();
+    }
+}
