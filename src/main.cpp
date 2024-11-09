899380b635ae6177bfbf770c84d39ca6af31b402
giannis <karelasgiannis@gmail.com>
2024-11-09 21:41:29 +0200

    Request all the AcubeSatparameters from ACDS

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,7 @@
 #include "definitions.h"
 #include "InitializationTask.hpp"
 #include "FreeRTOSHandlers.hpp"
+#include "RequestParamADCSTask.hpp"
 
 #define IDLE_TASK_SIZE 100
 
@@ -26,8 +27,10 @@ extern "C" void vApplicationGetIdleTaskMemory(StaticTask_t **ppxIdleTaskTCBBuffe
 extern "C" void main_cpp() {
     SYS_Initialize(NULL);
     initializationTask.emplace();
+    RequestParamADCSTask.emplace();
 
     initializationTask->createTask();
+    RequestParamADCSTask->createTask();
 
 
     vTaskStartScheduler();
