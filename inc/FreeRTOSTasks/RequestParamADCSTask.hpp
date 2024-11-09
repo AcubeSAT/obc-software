ee3a9904be84047ca07f09830bdd8786eb1ae8b0
giannis <karelasgiannis@gmail.com>
2024-11-09 11:10:08 +0200

    Request Parameters from ADCS through CAN

---
+++ inc/FreeRTOSTasks/RequestParamADCSTask.hpp
@@ -0,0 +1,33 @@
+#ifndef REQUESTPARAMADCS_HPP
+#define REQUESTPARAMADCS_HPP
+
+#endif //REQUESTPARAMADCS_HPP
+
+#pragma once
+
+#include "CAN/Driver.hpp"
+#include "Task.hpp"
+
+class RequestParamADCSTask : public Task {
+private:
+
+public:
+    const static inline uint16_t TaskStackDepth = 2300;
+
+    StackType_t taskStack[TaskStackDepth];
+
+    void execute();
+
+    RequestParamADCSTask() : Task("CAN Test") {}
+
+    /**
+     * Create freeRTOS Task
+     */
+
+     void createTask() {
+        xTaskCreateStatic(vClassTask < RequestParamADCS > , this->TaskName, RequestParamADCS::TaskStackDepth, this,
+                        tskIDLE_PRIORITY+2, this->taskStack, &(this->taskBuffer));
+    }
+};
+
+inline std::optional<RequestParamADCSTask> RequestParamADCSTask;
