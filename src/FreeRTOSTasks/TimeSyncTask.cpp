3b4e150a4cc27f2816e21fcdfee1334fd0eb10b1
forhs.m <forhsmarino@gmail.com>
2024-11-18 03:10:13 +0200

    Use of notifier

--- src/FreeRTOSTasks/TimeSyncTask.cpp
+++ src/FreeRTOSTasks/TimeSyncTask.cpp
@@ -4,8 +4,9 @@ void TimeSyncTask::execute() {
     while (true) {
         if (timeRequested == false) {
           vTaskDelay(pdMS_TO_TICKS(1000));
-            CAN::Application::createRequestParametersMessage(CAN::NodeIDs::ADCS, false,{AcubeSATParameters::ADCSOnBoardTime}, false);
-            ulTaskNotifyTake(pdTRUE, portMAX_DELAY);
+          AcubeSATParameters::adcsOnBoardTime.setNotifier(updateTimeOnReset);
+          CAN::Application::createRequestParametersMessage(CAN::NodeIDs::ADCS, false,{AcubeSATParameters::ADCSOnBoardTime}, false);
+          ulTaskNotifyTake(pdTRUE, portMAX_DELAY);
         } else {
             vTaskDelay(pdMS_TO_TICKS(100));
             CAN::Application::sendUTCTimeMessage();
