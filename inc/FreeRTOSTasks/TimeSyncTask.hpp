90b29d2fa5d8720aa8acd8f9081fe0b468c865cc
forhs.m <forhsmarino@gmail.com>
2024-11-19 15:15:52 +0200

    Proposed changes regarding timeSyncTask's emplacement

--- inc/FreeRTOSTasks/TimeSyncTask.hpp
+++ inc/FreeRTOSTasks/TimeSyncTask.hpp
@@ -33,14 +33,3 @@ public:
 };
 
 inline std::optional<TimeSyncTask> timeSyncTask;
-
-inline void updateTimeOnReset(const Time::DefaultCUC& time){
-    Time::DefaultCUC time1 = time;
-    UTCTimestamp utc = time1.toUTCtimestamp();
-    const int MonthOffset = -1;
-    tm now{utc.second, utc.minute, utc.hour, utc.day, utc.month + MonthOffset, utc.year, 0, 0, 0};
-    RTC_TimeSet(&now);
-    timeSyncTask->setTimeRequested(true);
-    xTaskNotifyGive(timeSyncTask->taskHandle);
-    AcubeSATParameters::adcsOnBoardTime.unsetNotifier();
-}
--- src/FreeRTOSTasks/TimeSyncTask.cpp
+++ src/FreeRTOSTasks/TimeSyncTask.cpp
@@ -1,22 +1,34 @@
 #include  "TimeSyncTask.hpp"
 
+static void updateTimeOnReset(const Time::DefaultCUC& time)
+{
+    Time::DefaultCUC time1 = time;
+    UTCTimestamp utc = time1.toUTCtimestamp();
+    const int MonthOffset = -1;
+    tm now{utc.second, utc.minute, utc.hour, utc.day, utc.month + MonthOffset, utc.year, 0, 0, 0};
+    RTC_TimeSet(&now);
+    timeSyncTask->setTimeRequested(true);
+    xTaskNotifyGive(timeSyncTask->taskHandle);
+    AcubeSATParameters::adcsOnBoardTime.unsetNotifier();
+}
+
 void TimeSyncTask::execute()
 {
-    while (true)
-    {
-        if (timeRequested == false)
-        {
+    while (true){
+        if (timeSyncTask.has_value()){
+            if (timeRequested == false){
+                vTaskDelay(pdMS_TO_TICKS(1000));
+                AcubeSATParameters::adcsOnBoardTime.setNotifier(updateTimeOnReset);
+                CAN::Application::createRequestParametersMessage(CAN::NodeIDs::ADCS, false,
+                                                                 {AcubeSATParameters::ADCSOnBoardTime}, false);
+                ulTaskNotifyTake(pdTRUE, portMAX_DELAY);
+            }else{
+                vTaskDelay(pdMS_TO_TICKS(100));
+                CAN::Application::sendUTCTimeMessage();
+                vTaskDelay(pdMS_TO_TICKS(TimeSyncPeriod));
+            }
+        }else{
             vTaskDelay(pdMS_TO_TICKS(1000));
-            AcubeSATParameters::adcsOnBoardTime.setNotifier(updateTimeOnReset);
-            CAN::Application::createRequestParametersMessage(CAN::NodeIDs::ADCS, false,
-                                                             {AcubeSATParameters::ADCSOnBoardTime}, false);
-            ulTaskNotifyTake(pdTRUE, portMAX_DELAY);
-        }
-        else
-        {
-            vTaskDelay(pdMS_TO_TICKS(100));
-            CAN::Application::sendUTCTimeMessage();
-            vTaskDelay(pdMS_TO_TICKS(TimeSyncPeriod));
         }
     }
 }
