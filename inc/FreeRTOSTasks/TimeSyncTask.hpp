50be9f0403c0c5f04d79c6a139634cb187932cfa
Forhs <forhsmarino@gmail.com>
2024-12-02 14:26:25 +0200

    Added the purpose of timeSyncTask in the .hpp file

--- inc/FreeRTOSTasks/TimeSyncTask.hpp
+++ inc/FreeRTOSTasks/TimeSyncTask.hpp
@@ -3,12 +3,18 @@
 #include "Task.hpp"
 #include "ApplicationLayer.hpp"
 
-/* timeSyncTask: As of now, (some features have to change or get added) checks if OBC has requested the on board time
+/* This task serves a dual purpose. The first objective is to request the onboard time from the ADCS subsystem if a
+ * reset has occured, enabling the OBC to synchronize and operate with the correct system time.
+ * The second objective is to periodically (at intervals defined by TimeSyncPeriod) invoke the sendUTCTimeMessage
+ * function to broadcast the onboard time to all subsystems.
+ * This ensures that subsystems remain synchronized, addressing any drift or desynchronization that may have occurred.
+ *
+ * timeSyncTask: As of now, (some features have to change or get added) checks if OBC has requested the onboard time
  * from the ADCS. If not, it sets a notifier at the adcsOnBoardTime. Then it request the adcsOnBoardTime from ADCS and
  * puts itself into the blocked state. If the time is requested then it sends a UTCTimeMessage which will get parsed
  * from the other subsystems so they will sync with the OBC.
  *
- *  adcsOnBoardTime Notifier: When adcsOnBoardTime gets updated, OBC sets the on board time through RTC, gives a notify
+ *  adcsOnBoardTime Notifier: When adcsOnBoardTime gets updated, OBC sets the onboard time through RTC, gives a notify
  *  to the timeSyncTask and unsets itself.
  */
 
