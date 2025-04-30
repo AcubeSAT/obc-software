542e4f2e0a8b21c4be5b87b8b347e196fd0b0bdd
Kyriakum <kyriakos186@gmail.com>
2025-04-30 14:26:30 +0300

    Moved all Services and ErrorHandler in common/

--- obc-dev/src/Platform/Services/ParameterStatisticsService.cpp
+++
@@ -1,8 +0,0 @@
-#include "Platform/Parameters/ParameterStatisticsService.hpp"
-
-void ParameterStatisticsService::initializeStatisticsMap() {
-    uint8_t idIndex = 0;
-    for (auto &statistic: ParameterStatistics::statistics) {
-        statisticsMap.insert({ParameterStatistics::statisticParameterIDs[idIndex++], statistic});
-    }
-}
